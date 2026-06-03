5cb2a9f47f67f4bd600989b22767138f4f92fd9e
Martin Alloatti <m4alloatti@gmail.com>
2026-06-03 15:56:26 -0300

    feat: agregar docker-compose unificado

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,10 +4,10 @@ import axios from "axios";
 const isDevelopment = import.meta.env.MODE === "development";
 
 const api = axios.create({
-  // 🎯 Cambiamos a 'api/' relativo al directorio actual del deploy para el proxy de Nginx
+  // Cambiamos a 'api/' relativo al directorio actual del deploy para el proxy de Nginx
   baseURL: isDevelopment 
     ? "http://127.0.0.1:8000/api/" 
-    : "api/", 
+    : "/api/", 
 });
 
 api.interceptors.request.use((config) => {
