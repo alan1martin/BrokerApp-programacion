259fef16148dee40ef9d8f2e8b572a398b67523c
Martin Alloatti <m4alloatti@gmail.com>
2026-06-03 15:45:14 -0300

    update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,14 +1,13 @@
 // src/services/api.js
 import axios from "axios";
 
-//  Detectamos si la app corre en tu compu (development) o en la academia (production)
 const isDevelopment = import.meta.env.MODE === "development";
 
 const api = axios.create({
-  // En tu casa usa localhost. En producción usa '/api' relativo para que Nginx redirija al backend
+  // 🎯 Cambiamos a 'api/' relativo al directorio actual del deploy para el proxy de Nginx
   baseURL: isDevelopment 
-    ? "http://127.0.0.1:8000/api" 
-    : "/api/", 
+    ? "http://127.0.0.1:8000/api/" 
+    : "api/", 
 });
 
 api.interceptors.request.use((config) => {
